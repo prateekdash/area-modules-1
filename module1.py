208c7546c2cb3729e22b2d0d9c2471da1f81faf5
prateekdash <31237414+prateekdash@users.noreply.github.com>
2017-11-21 10:44:30 +0530

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,6 @@
+def arearect(a,b):
+    arear=a*b
+    return arear
+def areasquare(a):
+    areas=a*a
+    return areas
---
+++ module1execution.py
@@ -0,0 +1,5 @@
+import module1
+print("Enter dimensions of the rectangle : ")
+r1=int(input("Enter the lenght of rectangle : "))
+r2=int(input("enter the width of the rectangle : "))
+print("Area of rectangle with entered dimension is : ",module1.arearect(r1,r2))
